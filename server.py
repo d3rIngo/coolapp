e967e824c5f900b2085ad37aea5f11ebc67b4304
d3rIngo <ingo.neubert92@gmx.de>
2024-02-14 11:17:56 +0100

    Coolapp

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+# a simple feature flag example
+weatherEndpointActive = False
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "OK"}), 200
+
+def weather_check():
+    # here we should can a weather API to retrieve the weather of a city
+    # and return the weather to the client
+    # this is a fake example 👇
+    return jsonify({"city": "Stuttgart", "temparature": "25", "unit": "celcius"}), 200
+
+if weatherEndpointActive:
+    app.add_url_rule('/weather', 'weather_check', weather_check, methods=['GET'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
